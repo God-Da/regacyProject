be54adecc67e422cc0b6a4dc0c849ac6a3146192
kdy <dev.dykang@gmail.com>
2026-09-04 17:26:32 +0900

    오류 수정

--- src/main/java/kr/or/oti/b01/dto/BoardImageDTO.java
+++ src/main/java/kr/or/oti/b01/dto/BoardImageDTO.java
@@ -1,6 +1,5 @@
 package kr.or.oti.b01.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,14 +10,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardImageDTO {
-	private String uuid;
-	private String filename;
-	private int ord;
-	
-	// S3 이미지 URL
+    private String uuid;
+    private String filename;
+    private int ord;
+    
+    // S3 이미지 URL
     private String imageUrl;
-	
-	public String getFullName() {
-		return this.uuid+"_"+this.filename;
-	}
+    
+    public String getFullName() {
+        return this.uuid + "_" + this.filename;
+    }
+
+    // 대소문자 매핑 호환용 (MyBatis fileName 대응)
+    public void setFileName(String fileName) {
+        this.filename = fileName;
+    }
+
+    public String getFileName() {
+        return this.filename;
+    }
 }
